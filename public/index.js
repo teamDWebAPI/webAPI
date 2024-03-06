1cd06a903323a58957dfbf2e3ef8950fe9135ce3
Taiga2022 <f2021212010@gmail.com>
2024-03-06 21:43:33 +0900

    fix-front

--- public/index.js
+++ public/index.js
@@ -1,17 +1,17 @@
-const dog=document.querySelector(".dog-selector")
 const fetchBtn=document.querySelector(".fetch-btn")
 const clearBtn=document.querySelector(".clear-btn")
-const jsonBlock=document.querySelector(".json-block")
+const jsonBlock=document.getElementById("json-block")
 
-async function getDogList(keyword, jsonBlock) {
-    const serverUrl = "http://localhost:8000/api/list";
+
+async function getDogList(query,keyword, jsonBlock) {
+    const serverUrl = "http://localhost:8000/api";
     try {
-        const response = await fetch(`${serverUrl}?keyword=${keyword}`,{ mode: "cors" });
+        const response = await fetch(`${serverUrl}/${query}?keyword=${keyword}`,{ mode: "cors" });
         if (!response.ok) {
             throw new Error(`Error fetching data: ${response.statusText}`);
         }
         const data = await response.json();
-        jsonBlock.innerHTML = JSON.stringify(data, null, 2);
+        jsonBlock.innerHTML = JSON.stringify(data, null, "\t");
     } catch (error) {
         console.error("Error:", error);
     }
@@ -21,15 +21,21 @@ async function getDogList(keyword, jsonBlock) {
 
 fetchBtn.addEventListener("click",(e)=>{
     e.preventDefault()
-    if(!dog.value){
-        alert("please select a dog")
-    }
-
-    getDogList(dog.value,jsonBlock)
+    removeJson(jsonBlock)
+    jsonBlock.classList.add("json-block")
+    const dogValue=document.querySelector(".dog-selector").value
+    const queryValue=document.querySelector(".query-selector").value
+    getDogList(queryValue,dogValue,jsonBlock)
 })
 
 
 clearBtn.addEventListener("click",(e)=>{
     e.preventDefault()
-    jsonBlock.innerHTML=""
+    removeJson(jsonBlock)
 })
+
+
+const removeJson=(jsonBlock)=>{
+    jsonBlock.classList.remove("json-block")
+    jsonBlock.innerHTML=""
+}
